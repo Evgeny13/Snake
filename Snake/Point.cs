fba25e28e6704c69bd3f520a35fa29595689ab9c
Evgeny <ste-eugeny@bk.ru>
2015-11-27 06:49:16 +0600

    создали класс Point

---
+++ Snake/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        public char cum;
+    }
+    public void Draw()
+    {
+        Console.SetCursorPosition(x, y); // выводит точку с координатами
+        Console.Write(sum);
+    }
+
+}
