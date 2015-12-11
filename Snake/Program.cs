9e1d360024d4572203287d0547743aec2fb5472b
Evgeny <ste-eugeny@bk.ru>
2015-12-11 10:15:32 +0600

    наследование фигуры

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -10,12 +10,27 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            Console.SetBufferSize(80, 25);
+
+            //отрисовка рамочки
+            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
+            WertikalLine leftLine = new WertikalLine(0, 24, 0, '+');            
+            WertikalLine righrLine = new WertikalLine(0, 24, 78, '+');
+            upLine.Drow();
+            downLine.Drow();
+            leftLine.Drow();
+            righrLine.Drow();
+
+
+            // отрисовка точек
             Point p1 = new Point(1, 3, '*'); // конструктор по созданию объекта
             p1.Draw();
 
             Point p2 = new Point(4, 5, '#');            
             p2.Draw();
 
+            // препятствия
             HorizontalLine hline = new HorizontalLine(5, 10, 8, '+');
             hline.Drow();
 
--- Snake/WertikalLine.cs
+++ Snake/WertikalLine.cs
@@ -6,26 +6,16 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    class WertikalLine
+    class WertikalLine : Figure
     {
-        List<Point> pList;
-
-        public WertikalLine(int yVerh, int yNis, int x, char sym)
+        public WertikalLine(int yUp, int yDown, int x, char sym)
         {
             pList = new List<Point>();
-            for (int y = yVerh; y <= yNis; y++)
+            for (int y = yUp; y <= yDown; y++)
             {
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
-        }
-        public void Drow()
-        {
-            foreach (Point p in pList)
-            {
-                p.Draw();
-            }
-        }
-
+        }      
     }
 }
