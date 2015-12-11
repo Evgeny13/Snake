c5211cc43e11bc82466ba7bd5403af09d324aac8
Evgeny <ste-eugeny@bk.ru>
2015-12-11 09:50:38 +0600

    Горизонтальная линия ДЗ-вертикальная линия

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -16,25 +16,12 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');            
             p2.Draw();
 
-            List<int> numList = new List<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
+            HorizontalLine hline = new HorizontalLine(5, 10, 8, '+');
+            hline.Drow();
 
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
+            WertikalLine wline = new WertikalLine(8, 15, 10, '+');
+            wline.Drow();
 
-            foreach (int i in numList)
-            {
-                Console.WriteLine(i);
-            }
-
-            numList.RemoveAt(0);
-
-            List<Point> pList = new List<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
 
             Console.ReadLine();
         }
---
+++ Snake/WertikalLine.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class WertikalLine
+    {
+        List<Point> pList;
+
+        public WertikalLine(int yVerh, int yNis, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yVerh; y <= yNis; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Drow()
+        {
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+
+    }
+}
